0000484118141c0fe9329bc5f4c6c75b28bd1ac8
Jinnrry <ok@xjiangwei.cn>
2019-10-20 15:30:12 +0800

    适配android10

--- Android/app/src/main/java/cn/xjiangwei/RobotHelper/Xposed/Hook4Input.java
+++ Android/app/src/main/java/cn/xjiangwei/RobotHelper/Xposed/Hook4Input.java
@@ -10,7 +10,48 @@ public class Hook4Input {
 
     public static void run(XC_LoadPackage.LoadPackageParam lpparam) {
 
-        //            //Hook安卓的输入相关类，为程序提权，使其能够点击其他应用
+        /**
+         * android 10输入参数改了
+         */
+        if ("10".equals(android.os.Build.VERSION.RELEASE)) {
+            Hook4Input.hook4android10(lpparam);
+        } else {
+            Hook4Input.hook4LTandroid10(lpparam);
+        }
+
+
+    }
+
+
+    /**
+     * 处理android10的hook
+     *
+     * @param lpparam
+     */
+    private static void hook4android10(XC_LoadPackage.LoadPackageParam lpparam) {
+        try {
+            XposedHelpers.findAndHookMethod("com.android.server.input.InputManagerService", lpparam.classLoader,
+                    "nativeInjectInputEvent",
+                    long.class, lpparam.classLoader.loadClass("android.view.InputEvent"), int.class, int.class, int.class, int.class, int.class,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.args[3] = 0;
+                            XposedBridge.log("拦截启动！long");
+                        }
+                    });
+        } catch (Exception e) {
+            XposedBridge.log("~~~~~~~~~~~类没找到~~~~~~~~~~~~~");
+        }
+    }
+
+
+    /**
+     * 处理android9及其以下版本的的hook
+     *
+     * @param lpparam
+     */
+    private static void hook4LTandroid10(XC_LoadPackage.LoadPackageParam lpparam) {
         try {
             XposedHelpers.findAndHookMethod("com.android.server.input.InputManagerService", lpparam.classLoader,
                     "nativeInjectInputEvent",
@@ -25,24 +66,6 @@ public class Hook4Input {
         } catch (Exception e) {
             XposedBridge.log("~~~~~~~~~~~类没找到~~~~~~~~~~~~~");
         }
-//            //Hook安卓的输入相关类，为程序提权，使其能够点击其他应用
-//            //网上都是这么写的，但是这样写并不能工作，或许某个安卓版本需要这样写吧？？？？
-//        try {
-//            XposedHelpers.findAndHookMethod("com.android.server.input.InputManagerService", lpparam.classLoader,
-//                    "nativeInjectInputEvent",
-//                    int.class, lpparam.classLoader.loadClass("android.view.InputEvent"), int.class, int.class, int.class, int.class, int.class, int.class,
-//                    new XC_MethodHook() {
-//                        @Override
-//                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//                            param.args[3] = 0;
-//                            XposedBridge.log("拦截启动！int");
-//                        }
-//                    });
-//        } catch (Exception e) {
-//            XposedBridge.log("~~~~~~~~~~~类没找到~~~~~~~~~~~~~");
-//        }
-
-
     }
 
 

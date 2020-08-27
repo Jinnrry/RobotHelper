f21168143f59412b5a7e9e12d6d716fa702155ca
jiangwei <ok@xjiangwei.cn>
2020-08-27 21:26:21 +0800

    添加pc cpu支持

--- sendevent/build.sh
+++ sendevent/build.sh
@@ -1,1 +1,4 @@
- GOOS=linux GOARCH=arm64 go build
+GOOS=linux GOARCH=arm64 go build -o ./bin/arm64/sendevent
+GOOS=linux GOARCH=arm go build -o ./bin/arm/sendevent
+GOOS=linux GOARCH=386 go build -o ./bin/x86/sendevent
+GOOS=linux GOARCH=amd64 go build -o ./bin/x64/sendevent
